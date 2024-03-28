7578af37b7e2b2d9c2a247c5854113fd7b35baf3
Mahaw <mahawprogramacao@gmail.com>
2024-03-28 16:49:41 -0300

    feat: global styles included

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Welcome from "./components/Welcome";
+import "./main.css";
+
 function App() {
   return (
     <div>
-      <h1>Olá Mundo!</h1>
+      <h1>Quiz de Programação</h1>
+      <Welcome />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
