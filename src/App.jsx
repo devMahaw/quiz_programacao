08ac9ab36607fc5ad28e0fd6b2fd6502c24fdeb0
Mahaw <mahawprogramacao@gmail.com>
2024-04-10 15:48:34 -0300

    feat: adding states to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { useContext } from "react";
+import { QuizContext } from "./context/quiz";
+import Question from "./components/Question";
 import Welcome from "./components/Welcome";
 import "./main.css";
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
   return (
     <div className="App">
       <h1>Quiz de Programação</h1>
-      <Welcome />
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Playing" && <Question />}
     </div>
   );
 }
