f383a8d1902560c88379dd6dc5fde5279ac6f411
Mahaw <mahawprogramacao@gmail.com>
2024-04-13 14:18:01 -0300

    feat: continuing Questions component

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -8,8 +8,13 @@ const Question = () => {
   console.log(quizState);
 
   return (
-    <div>
+    <div id="question">
       <p>Pergunta 0 de 10</p>
+      <h2>Pergunta atual</h2>
+      <div id="options-container">
+        <p>Opções</p>
+      </div>
+      <button>Continuar</button>
     </div>
   );
 };
