eec3331e3984901643db052a25ba95f3be9dd00d
Mahaw <mahawprogramacao@gmail.com>
2024-05-06 15:08:00 -0300

    main jsx change

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,5 @@ import "./global.css";
 import { QuizProvider } from "./context/quiz";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <QuizProvider>
-      <App />
-    </QuizProvider>
-  </React.StrictMode>
+  <React.StrictMode></React.StrictMode>
 );
