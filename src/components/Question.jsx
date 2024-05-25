0c08135247d27b12f6867ac46679cc843e4c209d
MateusADantas <mahawprogramacao@gmail.com>
2024-05-25 15:08:59 -0300

    feat: options included

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,7 +1,7 @@
-import React from "react";
 import { useContext } from "react";
 import { QuizContext } from "../context/quiz";
 import "./Question.css";
+import Option from "./Option";
 
 const Question = () => {
   const [quizState, dispatch] = useContext(QuizContext);
@@ -15,7 +15,9 @@ const Question = () => {
       </p>
       <h2>{currentQuestion.question}</h2>
       <div id="options-container">
-        <p>Opções</p>
+        {currentQuestion.options.map((option) => (
+          <Option option={option} key={option} />
+        ))}
       </div>
       <button onClick={() => dispatch({ type: "CHANGE_QUESTION" })}>
         Continuar
