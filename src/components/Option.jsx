2696ffd19b7f8d2d9c6b4c3bca8f1f80ff9839d6
MateusADantas <mahawprogramacao@gmail.com>
2024-05-26 15:50:28 -0300

    finished project

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -2,11 +2,16 @@ import { useContext } from "react";
 import { QuizContext } from "../context/quiz";
 import "./Option.css";
 
-const Option = ({ option }) => {
+const Option = ({ option, selectOption, answer }) => {
   const [quizState, dispatch] = useContext(QuizContext);
 
   return (
-    <div className="option">
+    <div
+      className={`option ${
+        quizState.answerSelected && option === answer ? "correct" : ""
+      } ${quizState.answerSelected && option !== answer ? "wrong" : ""}`}
+      onClick={() => selectOption()}
+    >
       <p>{option}</p>
     </div>
   );
